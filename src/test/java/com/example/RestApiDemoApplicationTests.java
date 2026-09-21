f253fc6584bee67b1f48727231d495dbb9eb7213
Carolina <carol18becerra@gmail.com>
2026-09-21 12:53:55 +0200

    todo bien arreglado

--- src/test/java/com/example/RestApiDemoApplicationTests.java
+++ src/test/java/com/example/RestApiDemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class RestApiDemoApplicationTests {
 
 	@Test
+	@DisplayName("Test para verificar que el contexto de Spring se levanta correctamente")
 	void contextLoads() {
 	}
 
---
+++ src/test/java/com/example/services/ProductServiceImplTest.java
@@ -0,0 +1,39 @@
+package com.example.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProductServiceImplTest {
+
+	@Test
+	void testFindAllPageable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindAllSort() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+}
